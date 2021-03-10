3d25b44369156b129afa53635f8bdaff08256a6d
Junpin <tyuighjk86@gmail.com>
2021-03-10 20:30:45 +0800

    homework customer

--- src/main/java/fju/edu/Customer.java
+++ src/main/java/fju/edu/Customer.java
@@ -1,4 +1,18 @@
 package fju.edu;
 
 public class Customer {
+    String id;
+    int cus;
+    int discount;
+
+    public Customer(String id, int cus, int discount){
+        this.id = id;
+        this.cus = cus;
+        this.discount = discount;
+
+    }
+    public void print(){
+        System.out.println(id + "\t" + cus + "\t" + discount + "\t" + (cus-discount));
+    }
+
 }
--- src/main/java/fju/edu/Test.java
+++ src/main/java/fju/edu/Test.java
@@ -1,4 +1,17 @@
 package fju.edu;
 
 public class Test {
+    public static void main(String[] args) {
+        Customer customer[] = {
+                new Customer("0013", 490, 0),
+                new Customer("0052", 1000, 100),
+                new Customer("0081", 290, 0),
+                new Customer("2122", 2000, 200)
+        };
+        System.out.println("客戶編號/消費金額/折讓/金額");
+        for (int i = 0; i < customer.length; i++) {
+            customer[i].print();
+
+        }
+    }
 }
